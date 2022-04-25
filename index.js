6d7fb354a5caf66d947ecf1a96e8725d3f7913be
Akilah Snowden <akilahdsnowden@gmail.com>
2022-04-25 16:43:20 -0400

    commit tree 8e2dfbc741c3b2aa534a48dc226b95e70d830c1f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code in this file!
+//define currentUser with const, expect to equal Grace Hopper
+const currentUser = 'Grace Hopper';
+//Define welcomeMessage, expected to include currentUser 
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+//Define excited welcome message, expected all caps and to include currentUser value
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+/*shortGreeting
+contains the first initial of the name stored in the 'currentUser' variable:
+AssertionError: expected 'Welcome, ' to contain 'G' */
+
+
+
+
+
+ 
